ffbc2b7f95886e61bb67cf71338db4a56f3d0cd8
tetriv0217 <tetriv0217@gmail.com>
2025-07-01 19:09:48 +0530

    Added git ignore

--- 1_Patterns/q2.cpp
+++ 1_Patterns/q2.cpp
@@ -1,15 +1,14 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
     cin>>n;
-    for(int i = 0;i<n;i++){
-        for(int j = 0;j<n;j++){
+    for(int i = 1;i<n+1;i++){
+        for(int j = 0;j<i;j++){
             cout<<"*";
         }
-        
         cout<<endl;
     }
     return 0;
